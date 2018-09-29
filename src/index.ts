bff7b0e7a47d33cbf62ee877762b55247cef0519
Sudhir <sudhiry@thoughtworks.com>
2018-09-29 20:37:44 +0530

    Added jquery library

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import  $ from  "jquery";
 class Demo {
     public message(): string {
         return "App is loaded!!!"
     }
 }
 console.log(new Demo().message());
+
+$('#dummyButton').on('click', () => alert('You have clicked me'));
