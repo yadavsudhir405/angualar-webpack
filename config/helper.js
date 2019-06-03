fcd7d8f1f7d2255a75efadede751339d1372b6a2
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-06-03 18:30:59 +0530

    Added

--- config/helper.js
+++ config/helper.js
@@ -6,7 +6,7 @@ const _root = path.resolve(__dirname, '..');
 
 function root(args) {
     args = Array.prototype.slice.call(arguments, 0);
-
+    console.log("Adding dummy log");
     return path.join.apply(path, [_root].concat(args));
 }
 
